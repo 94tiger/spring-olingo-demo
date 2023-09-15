5535cb0870847ec3db410261e47eef21126e7eee
94tiger <94tiger@naver.com>
2023-09-15 14:15:34 +0900

    first commit

---
+++ src/main/java/com/example/springolingodemo/SpringOlingoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springolingodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOlingoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOlingoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springolingodemo/SpringOlingoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springolingodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOlingoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
